b7e24bc95ef6f6ca3701830218dde6db6eb6ace1
chucshary <shary-princess@hotmail.com>
2014-07-19 17:45:41 -0500

    Reporte pruebas

--- src/Reportes/Reporte.java
+++ src/Reportes/Reporte.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplo;
+package Reportes;
 
 
 import java.awt.HeadlessException;
--- src/Reportes/principal.java
+++ src/Reportes/principal.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package ejemplo;
+package Reportes;
 
 import javax.swing.JFrame;
 
