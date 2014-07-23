77ed3a981685415bdf68517938a896b3fe87bdb4
chucshary <shary-princess@hotmail.com>
2014-07-22 20:51:00 -0500

    MUSIC INTRO

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,11 @@
 
-
+import Clases.Music;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
 /**
  *
  * @author Mariana Valencia
@@ -20,16 +17,17 @@ public class Server {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        try
-                {
-                    Thread.sleep(3000);
-                } catch (InterruptedException ex) {
-                 
-                }
+        try {
+            Clases.Music m = new Music();
+            m.reproduccion();
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+
+        }
 //        SuperUsuario.Serial m= new SuperUsuario.Serial();
-        callcenter.ServerMain m= new callcenter.ServerMain();
+        callcenter.ServerMain m = new callcenter.ServerMain();
         m.setLocationRelativeTo(null);
         m.setVisible(true);
     }
-    
+
 }
