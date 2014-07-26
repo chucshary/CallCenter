b4cdba15f201db5b45c55333d7568292157003b6
Mariana V <marivale_0@hotmail.com>
2014-07-26 12:03:36 -0500

    Revert "Revert "variablesbd""
    This reverts commit f521e5d3bcb8fd41289a8db0719a8e23adacc156.
    
    Conflicts:
    	src/EstructuraBD/variablesBD.java

--- src/EstructuraBD/variablesBD.java
+++ src/EstructuraBD/variablesBD.java
@@ -3,7 +3,8 @@ package EstructuraBD;
 public class variablesBD {
 public String baseDeDatos="call_center";
 public String usuarioMysql="root";
-public String passMysql="";  
+public String passMysql="softshary";  
+
 
   
 }
