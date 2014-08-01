c5653a39536f894b1620f14e01eb92bda7292216
Mariana V <marivale_0@hotmail.com>
2014-08-01 08:26:14 -0500

    l

--- src/EstructuraBD/variablesBD.java
+++ src/EstructuraBD/variablesBD.java
@@ -3,7 +3,7 @@ package EstructuraBD;
 public class variablesBD {
 public String baseDeDatos="call_center";
 public String usuarioMysql="root";
-public String passMysql="softshary";  
+public String passMysql="";  
 
   
 }
