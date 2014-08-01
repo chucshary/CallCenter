b56e83c4ce2c0a1579e102c7d87010e0d9896221
Jose-program-html <josey_8@hotmail.com>
2014-08-01 04:17:55 -0500

    lel

--- src/ConexionSocket/Hilo_Servidor.java
+++ src/ConexionSocket/Hilo_Servidor.java
@@ -55,6 +55,8 @@ public class Hilo_Servidor extends Thread {
                      envio2.writeUTF(opciones[1]);
                      opciones = metodo.respuesta(Integer.valueOf(recibo.readUTF()));
                  }
+                 envio.writeUTF(opciones[0]);
+                 envio2.writeUTF(opciones[1]);
                  int idmenu=Integer.valueOf(recibo.readUTF());
                  String nombre_cliente = recibo.readUTF();
                  String contacto = recibo.readUTF();
